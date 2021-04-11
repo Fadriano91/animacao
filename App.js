4ab7ba9232d9c612206933c442600de9eb41689a
Fadriano91 <fquimico@hotmail.com.br>
2021-04-11 02:08:01 -0300

    Commit Inicial- app animacao

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, StyleSheet, Text, TouchableOpacity, Alert, Platform } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+import Writer from './components/Writer'
+
+const TAMANHO_CIRCULO = 100
+
+
+const Circulo = ({ onPress }) => {
+  return (
+    <View style={[StyleSheet.absoluteFillObject, styles.containerCirculo]}>
+      <Text style={styles.titulo}>Animações em React Native</Text>
+      <Writer />
+      <TouchableOpacity onPress={onPress}>
+        <View style={[styles.circulo]}>
+          <AntDesign name="arrowright" size={28} color={"#8c4227"} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
 
 export default function App() {
+  const onPress = () => {
+    let mensagem = "Você clicou! 👏"
+    if (Platform.OS === 'web') {
+      alert(mensagem)
+    } else {
+      Alert.alert(
+        'Aviso',
+        mensagem,
+        [{
+          text: 'Cancelar',
+          onPress: () => console.log('Pressionou o cancelar'),
+          style: 'cancel'
+        },
+        {
+          text: 'Ok',
+          onPress: () => console.log('Pressionou o Ok')
+        }], {
+        cancelable: false //Tira o comportamento de cancelar clicando em qualquer lugar
+      })
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Circulo onPress={onPress} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F2CDAC",
+    alignItems: 'center'
   },
-});
+  titulo: {
+    fontSize: 25,
+    color: "#D99873",
+    paddingTop: 20
+  },
+  containerCirculo: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: TAMANHO_CIRCULO
+  },
+  circulo: {
+    backgroundColor: "#D3367F",
+    width: TAMANHO_CIRCULO,
+    height: TAMANHO_CIRCULO,
+    borderRadius: TAMANHO_CIRCULO / 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
